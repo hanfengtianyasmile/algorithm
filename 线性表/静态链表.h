7fc27010f87e67aa8eebd4726405efe37457560c
hanfengtianyasmile <ljgphp@163.com>
2015-09-14 19:07:52 +0800

    静态链表完成

--- 线性表/静态链表.h
+++ 线性表/静态链表.h
@@ -5,22 +5,24 @@ typedef struct {
 	int cur;
 }SList;
 
-
+typedef struct{
+	SList list[N];
+}SLinkList;
 
 //初始化
-void init_sl(SList slist[]){
+void init_sl(SLinkList *L){
 	int i;
 	for(i=0;i<N-1;i++){
-		slist[i].cur = i+1;
+		L->list[i].cur = i+1;
 	}
-	slist[N-1].cur = 0;
+	L->list[N-1].cur = 0;
 } 
  
 //申请分配一个空闲节点
-int malloc_sl(SList slist[]){
-	int i = slist[0].cur;
+int malloc_sl(SLinkList *L){
+	int i = L->list[0].cur;
 	if(1){
-		slist[0].cur = slist[i].cur;
+		L->list[0].cur = L->list[i].cur;
 	}
 	return i;
 } 
@@ -28,15 +30,62 @@ int malloc_sl(SList slist[]){
 
 //释放一个空闲点到空闲链表
 
-void free_sl(SList slist[],int k){
-	slist[k].cur = slist[0].cur;
-	slist[0].cur = k;
+void free_sl(SLinkList *L,int k){
+	L->list[k].cur = L->list[0].cur;
+	L->list[0].cur = k;
 }
 
 
 
-//未完待续 
+//在第i个元素之前插入新的数据元素e
+int ListInsert(SLinkList *L,int i,char e){
+	int j,k,l;
+	k = N -1;
+	if(i < 1 || i > N){
+		return 0;
+	}
+	
+	j = malloc_sl(L);
+	
+	if(j){
+		L->list[j].data = e;
+		for(l = 1;l <= i-1;l++){
+			k = L->list[k].cur;
+		}
+		
+		L->list[j].cur = L->list[k].cur;
+		L->list[k].cur = j;
+		
+		return 1;
+		
+	}
+	
+	return 0;
+	
+} 
 
+//删除在L中的第i个元素
+int ListDelete(SLinkList *L,int i){
+	int j,k;
+	if(i < 1 || i > N){
+		return 0;
+	}
+	
+	k = N - 1;
+	
+	for(j = 1;j <= i-1; j++){
+		k = L->list[k].cur;
+	}
+	j = L->list[k].cur;
+	
+	L->list[k].cur = L->list[j].cur;
+	
+	free_sl(L,j);
+	
+	return 1;
+	
+	
+} 
 
 
 
